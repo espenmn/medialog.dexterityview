82c5abb03ddfb42b84120fe2ec9c6eedda08abde
Espen Moe-Nilssen <espen@medialog.no>
2015-07-13 23:48:13 +0200

    vocabulary + leadimage reg

--- medialog/dexterityview/vocabulary.py
+++ medialog/dexterityview/vocabulary.py
@@ -14,20 +14,27 @@ def format_size(size):
 
 
 def ImageSizeVocabulary(context):
-    portal_properties = api.portal.get_tool(name='portal_properties')
-
-    if 'imaging_properties' in portal_properties.objectIds():
-        sizes = portal_properties.imaging_properties.getProperty('allowed_sizes')
-        sizes += ('original',)
-        terms = [ SimpleTerm(value=format_size(pair), token=format_size(pair), title=pair) for pair in sizes ]
-        return SimpleVocabulary(terms)
-    else:
-        return SimpleVocabulary([
+    #default vocabulary
+    terms = [
             SimpleTerm('mini', 'mini', u'Mini'),
             SimpleTerm('preview', 'preview', u'Preview'),
             SimpleTerm('large', 'large', u'Large'),
             SimpleTerm('original', 'original', u'Original'),
-        ])  
+        ]
+        
+    #Plone 5
+    try:
+        sizes = api.portal.get_registry_record('plone.allowed_sizes')
+    
+    #Plone 4
+    portal_properties = api.portal.get_tool(name='portal_properties')
+    if 'imaging_properties' in portal_properties.objectIds():
+        sizes = portal_properties.imaging_properties.getProperty('allowed_sizes')
+
+    if sizes:
+        if not 'original' in sizes:
+            sizes += ('original',)
+        terms = [ SimpleTerm(value=format_size(pair), token=format_size(pair), title=pair) for pair in sizes ]
       
     return SimpleVocabulary(terms)
 
